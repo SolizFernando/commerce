1bca9e8a9a7b43a86bb6380e5cd8cc5f994e4e42
SolizFernando <166055762+SolizFernando@users.noreply.github.com>
2025-01-03 12:56:02 -0300

    Proyecto vacío

---
+++ commerce/src/main/java/ar/edu/unju/fi/CommerceApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceApplication.class, args);
+	}
+
+}
